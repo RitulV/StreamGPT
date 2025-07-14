01ac15b3772544ce3ea20377420a6b77eb778d22
“RitulV” <ritulcodes@gmail.com>
2025-07-14 21:43:23 +0530

    changed login and signup pages

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import { BASE_IMG_URL } from "../assets/constants";
 import { Link } from "react-router-dom";
 
 const SignIn = () => {
+  const [checked, setChecked] = useState<boolean>(false);
+
   return (
     <div className="bg-radial from-[#282b36] to-[#01030f] text-amber-50 flex flex-col w-[100vw] h-[100vh]">
       <div className="m-1.5 max-h-fit p-0.5 ">
-        <img
-          className="object-cover ml-4 h-15 w-30"
-          src={BASE_IMG_URL + "stream.png"}
-        />
+        <Link to="/">
+          <img
+            className="object-cover ml-4 h-15 w-30 "
+            src={BASE_IMG_URL + "stream.png"}
+          />
+        </Link>
       </div>
 
       <div className="self-center mt-20 w-[400px]">
@@ -41,14 +46,6 @@ const SignIn = () => {
                 <label className="block text-sm/6 font-medium text-white">
                   Password
                 </label>
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-semibold text-sky-500 hover:text-sky-300"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
               </div>
               <div className="mt-2">
                 <input
@@ -63,9 +60,32 @@ const SignIn = () => {
             </div>
 
             <div>
+              <div className="flex items-center justify-between">
+                <div className="grid grid-cols-[1rem_auto] items-center gap-x-2">
+                  <input
+                    id="remember-me"
+                    type="checkbox"
+                    checked={checked}
+                    onChange={(e) => setChecked(e.target.checked)}
+                    className="h-4 w-4"
+                  />
+                  <label htmlFor="remember-me" className="text-sm text-white">
+                    Remember Me
+                  </label>
+                </div>
+                <div>
+                  <Link
+                    to={""}
+                    className="text-sm text-cyan-600 hover:text-cyan-500"
+                  >
+                    Forgot password?
+                  </Link>
+                </div>
+              </div>
+
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-sky-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-sky-300 cursor-pointer focus-visible:outline-2 focus-visible:outline-offset-2"
+                className="mt-4 flex w-full justify-center rounded-md bg-sky-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-sky-300 cursor-pointer focus-visible:outline-2 focus-visible:outline-offset-2"
               >
                 Continue
               </button>
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import { BASE_IMG_URL } from "../assets/constants";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [agreed, setAgreed] = useState<boolean>(false);
   return (
     <div className="bg-radial from-[#282b36] to-[#01030f] text-amber-50 flex flex-col w-[100vw] h-[100vh]">
       <div className="m-1.5 max-h-fit p-0.5 ">
-        <img
-          className="object-cover ml-4 h-15 w-30 "
-          src={BASE_IMG_URL + "stream.png"}
-        />
+        <Link to="/">
+          <img
+            className="object-cover ml-4 h-15 w-30 "
+            src={BASE_IMG_URL + "stream.png"}
+          />
+        </Link>
       </div>
 
       <div className="self-center mt-20">
@@ -21,9 +25,7 @@ const SignUp = () => {
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
             <div>
-              <label
-                className="block text-sm/6 font-medium text-white"
-              >
+              <label className="block text-sm/6 font-medium text-white">
                 Email address
               </label>
               <div className="mt-2">
@@ -40,9 +42,7 @@ const SignUp = () => {
 
             <div>
               <div className="flex items-center justify-between">
-                <label
-                  className="block text-sm/6 font-medium text-white"
-                >
+                <label className="block text-sm/6 font-medium text-white">
                   Password
                 </label>
               </div>
@@ -59,9 +59,32 @@ const SignUp = () => {
             </div>
 
             <div>
+              <div className="flex items-center justify-between">
+                <div className="grid grid-cols-[1rem_auto] items-center gap-x-2">
+                  <input
+                    id="agreed"
+                    type="checkbox"
+                    checked={agreed}
+                    onChange={(e) => setAgreed(e.target.checked)}
+                    className="h-4 w-4"
+                  />
+                  <label htmlFor="agreed" className="text-sm text-white">
+                    I agree to the{" "}
+                    <Link to={""} className="underline underline-offset-3">
+                      terms and conditions
+                    </Link>
+                  </label>
+                </div>
+              </div>
+
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-sky-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-sky-300 cursor-pointer focus-visible:outline-2 focus-visible:outline-offset-2"
+                disabled={!agreed}
+                className={
+                  agreed
+                    ? "mt-4 flex w-full justify-center rounded-md bg-sky-500 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-sky-300 cursor-pointer focus-visible:outline-2 focus-visible:outline-offset-2"
+                    : "mt-4 flex w-full justify-center rounded-md px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs bg-sky-300 focus-visible:outline-2 focus-visible:outline-offset-2"
+                }
               >
                 Continue
               </button>
